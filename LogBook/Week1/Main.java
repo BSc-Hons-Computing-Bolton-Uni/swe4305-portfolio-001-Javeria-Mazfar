204255f9c73dfc419fab258f0bd7febe0e9fee6c
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-01-29 22:24:56 +0000

    testing

--- LogBook/Week1/Main.java
+++ LogBook/Week1/Main.java
@@ -1,12 +1,18 @@
 package LogBook.Week1;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
 
-    public static void main(String[] args)
-    {
+        double average = (num1 + num2) / 2;
 
-        System.out.println("Hello");
-
+        System.out.println("The average is: " + average);
     }
-
 }
+
