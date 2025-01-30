7a304e5f3c804e35c273275432d92ab62ad6d962
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-01-30 22:32:18 +0000

    testing

--- LogBook/Week1/Main.java
+++ LogBook/Week1/Main.java
@@ -5,21 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first integer: ");
-        int num1 = scanner.nextInt();
-        System.out.print("Enter second integer: ");
-        int num2 = scanner.nextInt();
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
 
-        System.out.println("Original Order: " + num1 + " " + num2);
+        int currentYear = 2025;  // Change if the current year changes
+        int yearOfBirth = currentYear - age;
 
-        // Swapping
-        int temp = num1;
-        num1 = num2;
-        num2 = temp;
-
-        System.out.println("Reversed Order: " + num1 + " " + num2);
+        System.out.println("You were born in " + yearOfBirth);
     }
 }
+
 
 
 
