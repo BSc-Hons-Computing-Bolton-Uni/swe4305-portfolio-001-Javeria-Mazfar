3439ea9e46459b14c69666d30db1124c520dc21c
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-01-30 22:07:39 +0000

    testing

--- LogBook/Week1/Main.java
+++ LogBook/Week1/Main.java
@@ -5,11 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.println("Hello " + name + "!");
+        System.out.print("Enter length: ");
+        double length = scanner.nextDouble();
+        System.out.print("Enter height: ");
+        double height = scanner.nextDouble();
+
+        double perimeter = 2 * (length + height);
+        double area = length * height;
+
+        System.out.println("Perimeter: " + perimeter);
+        System.out.println("Area: " + area);
     }
 }
+
 
 
 
