be601df12f4242faa0be935bb59f3a86d14fb00c
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-01-30 22:27:57 +0000

    testing

--- LogBook/Week1/Main.java
+++ LogBook/Week1/Main.java
@@ -5,15 +5,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        double num1 = scanner.nextDouble();
-        System.out.print("Enter second number: ");
-        double num2 = scanner.nextDouble();
+        System.out.print("Enter first integer: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter second integer: ");
+        int num2 = scanner.nextInt();
 
-        double average = (num1 + num2) / 2;
-        System.out.println("Average: " + average);
+        System.out.println("Original Order: " + num1 + " " + num2);
+
+        // Swapping
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("Reversed Order: " + num1 + " " + num2);
     }
 }
+
 
 
 
