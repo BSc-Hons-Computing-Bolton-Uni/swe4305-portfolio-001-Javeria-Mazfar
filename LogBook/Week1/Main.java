dc24ad741024d773f618fe3ecd019330230387e5
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-01-30 22:40:43 +0000

    testing

--- LogBook/Week1/Main.java
+++ LogBook/Week1/Main.java
@@ -1,26 +1,19 @@
 package LogBook.Week1;
 
 import java.util.Scanner;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of feet: ");
+        double feet = scanner.nextDouble();
 
-        System.out.print("Enter your date of birth (dd/MM/yyyy): ");
-        String dob = scanner.nextLine();
+        double miles = feet / 5280;
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate birthDate = LocalDate.parse(dob, formatter);
-        LocalDate currentDate = LocalDate.now();
-
-        long daysOld = ChronoUnit.DAYS.between(birthDate, currentDate);
-
-        System.out.println("You are " + daysOld + " days old.");
+        System.out.println(feet + " feet is equal to " + miles + " miles.");
     }
 }
+
 
 
 
