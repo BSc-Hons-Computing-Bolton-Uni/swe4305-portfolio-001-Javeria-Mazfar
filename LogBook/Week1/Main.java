87d44ac96066fd6745bc3bd6b33680cfea44ac88
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-01-30 22:36:46 +0000

    testing

--- LogBook/Week1/Main.java
+++ LogBook/Week1/Main.java
@@ -1,19 +1,27 @@
 package LogBook.Week1;
 
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
 
-        int currentYear = 2025;  // Change if the current year changes
-        int yearOfBirth = currentYear - age;
+        System.out.print("Enter your date of birth (dd/MM/yyyy): ");
+        String dob = scanner.nextLine();
 
-        System.out.println("You were born in " + yearOfBirth);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate birthDate = LocalDate.parse(dob, formatter);
+        LocalDate currentDate = LocalDate.now();
+
+        long daysOld = ChronoUnit.DAYS.between(birthDate, currentDate);
+
+        System.out.println("You are " + daysOld + " days old.");
     }
 }
+
 
 
 
