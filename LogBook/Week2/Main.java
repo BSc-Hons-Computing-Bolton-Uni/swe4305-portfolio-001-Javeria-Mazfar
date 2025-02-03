efc15fdf1d1e3cb14083711eac8a53c63bfe8aa2
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-03 11:21:00 +0000

    test

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -12,7 +12,7 @@ class GFG {
         // looping from 1 to 10 to print the multiplication
         // table of the number.
         // using for loop
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 1; i <= 12; i++) {
             // printing the N*i,ie ith multiple of N.
             System.out.println(N + " * " + i + " = "
                     + N * i);
