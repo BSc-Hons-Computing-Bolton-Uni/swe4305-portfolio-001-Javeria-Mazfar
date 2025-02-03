5d2bd6331441779549c9ba58a316029b8dbcd0de
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-03 11:15:25 +0000

    test

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -1,28 +1,22 @@
 package LogBook.Week2;
 
-import java.util.Scanner;
+// Java Program to print the multiplication table of the number N using for loop
 
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
+class GFG {
+    public static void main(String[] args)
+    {
+        // number n for which we have to print the
+        // multiplication table.
+        int N = 7;
 
-        int currentYear = 2025;  // Change if the current year changes
-        int yearOfBirth = currentYear - age;
-
-        System.out.println("You were born in " + yearOfBirth);
-
-        if(age >= 18)
-
-            System.out.println("You're 18 years old or over.");
-
-          else
-
-             System.out.println("You're under 18.");
-
-
-
+        // looping from 1 to 10 to print the multiplication
+        // table of the number.
+        // using for loop
+        for (int i = 1; i <= 10; i++) {
+            // printing the N*i,ie ith multiple of N.
+            System.out.println(N + " * " + i + " = "
+                    + N * i);
         }
     }
+}
 
