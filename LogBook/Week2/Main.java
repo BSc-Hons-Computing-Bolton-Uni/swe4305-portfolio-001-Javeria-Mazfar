1dbdcd06131fd956b02145078f31bad029c3a674
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-03 10:26:39 +0000

    test

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -1,4 +1,22 @@
 package LogBook.Week2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = scanner.nextInt();
+
+        int currentYear = 2025;  // Change if the current year changes
+        int yearOfBirth = currentYear - age;
+
+        System.out.println("You were born in " + yearOfBirth);
+
+        if(age >= 18) {
+
+            System.out.println("You're 18 years old or over");
+
+        }
+    }
 }
