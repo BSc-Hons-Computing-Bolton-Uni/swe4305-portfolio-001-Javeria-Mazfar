8383cde0b560f1be44960958f1c0b26adcb71267
Javeria-Mazfar <100190217@student.burnley.ac.uk>
2025-02-04 13:59:04 +0000

    test

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -1,21 +1,4 @@
 package LogBook.Week2;
 
-// Java Program to print the multiplication table of the number N using for loop
-
-class GFG {
-    public static void main(String[] args)
-    {
-        // number n for which we have to print the
-        // multiplication table.
-        int N = 7;
-
-        // looping from 1 to 10 to print the multiplication
-        // table of the number.
-        // using for loop
-        for (int i = 1; i <= 12; i++) {
-            // printing the N*i,ie ith multiple of N.
-            System.out.println(N + " * " + i + " = " + N * i);
-        }
-    }
+public class Main {
 }
-
