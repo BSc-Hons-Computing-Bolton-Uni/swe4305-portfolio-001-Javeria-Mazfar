0556f9bd0ada75e63f1c2927b5f47401ae996d89
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-07 19:04:40 +0000

    testing

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -1,4 +1,37 @@
 package LogBook.Week2;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your letter grade (A-F): ");
+        char grade = scanner.next().charAt(0);
+
+        // Convert letter grade to university grades
+        switch (grade) {
+            case 'A':
+                System.out.println("1st");
+                break;
+            case 'B':
+                System.out.println("2:1");
+                break;
+            case 'C':
+                System.out.println("2:2");
+                break;
+            case 'D':
+                System.out.println("3rd");
+                break;
+            case 'E':
+                System.out.println("ordinary");
+                break;
+            case 'F':
+                System.out.println("fail");
+                break;
+            default:
+                System.out.println("Invalid grade");
+                break;
+        }
+    }
 }
