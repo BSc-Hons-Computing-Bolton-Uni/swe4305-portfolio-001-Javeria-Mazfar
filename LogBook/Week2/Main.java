a4caba52d3b01a45352d88423155a6ab290754c8
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-10 10:00:15 +0000

    test

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -1,13 +1,21 @@
 package LogBook.Week2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Loop to print the seven times multiplication table
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number for the times table: ");
+        int num = scanner.nextInt();
+
+        // Loop to print the chosen times multiplication table
         for (int i = 1; i <= 12; i++) {
-            System.out.println(i + " x 7 = " + (i * 7));
+            System.out.println(i + " x " + num + " = " + (i * num));
         }
     }
 }
+
 
 
 
