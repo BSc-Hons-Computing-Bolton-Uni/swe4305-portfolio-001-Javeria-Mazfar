b6476a1306669565f0f991d3877e0f1204ff2029
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-10 10:03:58 +0000

    test

--- LogBook/Week2/Main.java
+++ LogBook/Week2/Main.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // Create a Scanner object to read input
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the number for the times table: ");
-        int num = scanner.nextInt();
+        char choice;
 
-        // Loop to print the chosen times multiplication table
-        for (int i = 1; i <= 12; i++) {
-            System.out.println(i + " x " + num + " = " + (i * num));
-        }
+        do {
+            // Ask the user for a number to print its times table
+            System.out.print("Enter the number for the times table: ");
+            int num = scanner.nextInt();
+
+            // Loop to print the chosen times multiplication table
+            for (int i = 1; i <= 12; i++) {
+                System.out.println(i + " x " + num + " = " + (i * num));
+            }
+
+            // Ask if the user wants to continue
+            System.out.print("Do you want to see another times table? (Y/N): ");
+            choice = scanner.next().charAt(0);
+        } while (choice == 'Y' || choice == 'y');
+
+        // Print goodbye message if the user decides to stop
+        System.out.println("Goodbye!");
     }
 }
 
