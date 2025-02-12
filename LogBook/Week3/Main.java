63daea2499ffc343c3f42ca20eab93f02bfab302
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-12 22:17:54 +0000

    testing

--- LogBook/Week3/Main.java
+++ LogBook/Week3/Main.java
@@ -5,7 +5,15 @@ public class Main {
         // Instantiate a Student object with ID and name
         Student student = new Student(100190217, "Javeria");
 
+        // Instantiate a Course object with code and name
+        Course course = new Course("(A1007-1001)", "BOLTON Computing");
+
+        // Enrol the student in the course
+        student.enrol(course);
+
         // Call the print method to test the values
         student.print();
     }
 }
+
+
