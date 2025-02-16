43f872f00ff7ca5a0b885dd9879b8dce7823531f
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-16 22:56:44 +0000

    testing

--- LogBook/Week3/Student.java
+++ LogBook/Week3/Student.java
@@ -1,9 +1,10 @@
 package LogBook.Week3;
+import java.util.Random;
 
 class Student {
 
     // Attributes
-     int id;
+    int id;
     String name;
     Course course;
     int[] marks;  // Array to store four integer marks
@@ -13,8 +14,19 @@ class Student {
     public Student(int id, String name, String[] modules, int[] marks) {
         this.id = id;
         this.name = name;
-        this.marks = new int[4];  // Initialize the marks array to store four integer mark
+        this.marks = new int[4];  // Initialize the marks array to store four integer marks
         this.modules = modules;
+
+        // Assign random marks to each module using the random number generator
+        generateRandomMarks();
+    }
+
+    // Method to generate random marks between 0 and 100
+    private void generateRandomMarks() {
+        Random rand = new Random();
+        for (int i = 0; i < marks.length; i++) {
+            marks[i] = rand.nextInt(101);  // Generates a random number between 0 and 100 (inclusive)
+        }
     }
 
     // Print method
@@ -43,4 +55,5 @@ class Student {
         this.course = course;
     }
 }
+
 
