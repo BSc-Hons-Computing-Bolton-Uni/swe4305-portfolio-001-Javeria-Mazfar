dbe5a677d63e6237517d15911eb306588282eaa7
Javeria Mazfar <100190217@student.burnley.ac.uk>
2025-02-16 21:55:28 +0000

    testing

--- LogBook/Week3/Student.java
+++ LogBook/Week3/Student.java
@@ -6,11 +6,13 @@ class Student {
      int id;
     String name;
     Course course;
+    int[] marks;  // Array to store four integer marks
 
     // Constructor
     public Student(int id, String name) {
         this.id = id;
         this.name = name;
+        this.marks = new int[4];  // Initialize the marks array to store four integer mark
     }
 
     // Print method
